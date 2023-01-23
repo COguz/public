3729aeb1add5103f68f85b22f2274cd7bda1a555
COguz <coguz9812m@gmail.com>
2023-01-23 22:16:33 +0300

    arch again

--- Scripts/xinput.sh
+++ Scripts/xinput.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 xinput set-prop "CUST0001:00 04F3:30AA Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "CUST0001:00 04F3:30AA Touchpad" "libinput Natural Scrolling Enabled" 0
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled" 0
